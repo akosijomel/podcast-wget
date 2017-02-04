eb172a4fe79ccfdfd56802816eb31168d42776c9
jomel <jomel.madia@gmail.com>
2017-02-04 14:29:59 +0800

    add set directory

--- get_podcast.sh
+++ get_podcast.sh
@@ -1,14 +1,23 @@
 
 #!/bin/bash
+# Enter URL of podcast 
+# Usually from RSS links
 echo "ENTER URL OF PODCAST: "
 read URL
-CURL=`curl $URL`
 
+# Directory name 
+echo "ENTER FOLDER NAME: "
+read FOLDER_NAME
+
+# Create Directory to downlaod
+mkdir ~/Music/$FOLDER_NAME
+CURL=`curl $URL`
 
 EXTRACTED_MP3=`echo $CURL | grep -Po '(?<=url=")[^"]*(?=")' | grep .mp3`
 
 for LINK in $EXTRACTED_MP3
 do
-	`wget $LINK`
+	echo DOWNLOAD...Plese Wait
+	wget -P ~/Music/$FOLDER_NAME -o ~/Music/$FOLDER_NAME/log.txt $LINK
 done
 
